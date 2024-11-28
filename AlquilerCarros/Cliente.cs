a55e1b0f0de736b4ed81cf6dd19bcd4c1c9c5875
SebastianGarb <sebastiangarboza63@hotmail.com>
2024-11-28 09:56:01 -0500

    seb

---
+++ AlquilerCarros/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerCarros
+{
+    
+        public class Cliente
+        {
+            public int Dni { get; set; }
+            public string Nombre { get; set; }
+            public double PagoMaximo { get; set; }
+
+            public Cliente(int dni, string nombre, double pagomaximo)
+            {
+                Dni = dni;
+                Nombre = nombre;
+                PagoMaximo = pagomaximo;
+            }
+            public override string ToString()
+            {
+                return $"Dni: {Dni} \nNombre: {Nombre} \nPago Maximo:s/{PagoMaximo}";
+            }
+        }
+    
+}
